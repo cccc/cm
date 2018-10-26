28bb3d6368a2d197ab705df9978a6cb29c5a2979
Florian Zeitz <florob@babelmonkeys.de>
2018-10-26 22:31:56 +0200

    Sonoj2018: Initial config

--- ansible/lookup_plugins/keepass.py
+++ ansible/lookup_plugins/keepass.py
@@ -5,8 +5,8 @@ import os
 import sys
 import getpass
 
-reload(sys)
-sys.setdefaultencoding('UTF8')
+# reload(sys)
+# sys.setdefaultencoding('UTF8')
 
 from ansible.errors import AnsibleError
 from ansible.plugins.lookup import LookupBase
